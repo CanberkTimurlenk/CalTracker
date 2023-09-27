ee9abd497f23b36349ea28bb0d9d2d6eb036560c
batuhanbora <batuhanbora.alaftar@bilgeadamboost.com>
2023-09-27 20:04:36 +0300

    Add Login, Register, Verification

---
+++ FormUI/FormRegisterAim.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace FormUI
+{
+    partial class FormRegisterAim
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label3 = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            registrationButton = new Button();
+            clearButton = new Button();
+            label5 = new Label();
+            label6 = new Label();
+            kryptonButton1 = new Krypton.Toolkit.KryptonButton();
+            kryptonNumericUpDown1 = new Krypton.Toolkit.KryptonNumericUpDown();
+            kryptonComboBox1 = new Krypton.Toolkit.KryptonComboBox();
+            label7 = new Label();
+            kryptonNumericUpDown2 = new Krypton.Toolkit.KryptonNumericUpDown();
+            txtPassword = new TextBox();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            label8 = new Label();
+            textBox1 = new TextBox();
+            textBox3 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)kryptonComboBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("MS UI Gothic", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.FromArgb(32, 191, 107);
+            label3.Location = new Point(25, 30);
+            label3.Name = "label3";
+            label3.Size = new Size(155, 27);
+            label3.TabIndex = 0;
+            label3.Text = "Get Started";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(36, 220);
+            label1.Name = "label1";
+            label1.Size = new Size(33, 17);
+            label1.TabIndex = 1;
+            label1.Text = "Aim";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(33, 262);
+            label2.Name = "label2";
+            label2.Size = new Size(78, 17);
+            label2.TabIndex = 1;
+            label2.Text = "Heigth(cm)";
+            // 
+            // registrationButton
+            // 
+            registrationButton.BackColor = Color.FromArgb(32, 191, 107);
+            registrationButton.Cursor = Cursors.Hand;
+            registrationButton.FlatAppearance.BorderSize = 0;
+            registrationButton.FlatStyle = FlatStyle.Flat;
+            registrationButton.ForeColor = Color.White;
+            registrationButton.Location = new Point(30, 351);
+            registrationButton.Name = "registrationButton";
+            registrationButton.Size = new Size(218, 28);
+            registrationButton.TabIndex = 4;
+            registrationButton.Text = "REGISTER";
+            registrationButton.UseVisualStyleBackColor = false;
+            // 
+            // clearButton
+            // 
+            clearButton.BackColor = Color.White;
+            clearButton.Cursor = Cursors.Hand;
+            clearButton.Enabled = false;
+            clearButton.FlatAppearance.BorderSize = 2;
+            clearButton.FlatStyle = FlatStyle.Flat;
+            clearButton.ForeColor = Color.FromArgb(32, 191, 107);
+            clearButton.Location = new Point(30, 398);
+            clearButton.Name = "clearButton";
+            clearButton.Size = new Size(218, 28);
+            clearButton.TabIndex = 4;
+            clearButton.Text = "CLEAR";
+            clearButton.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(59, 438);
+            label5.Name = "label5";
+            label5.Size = new Size(161, 17);
+            label5.TabIndex = 5;
+            label5.Text = "Already have an Account";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Cursor = Cursors.Hand;
+            label6.ForeColor = Color.FromArgb(165, 177, 194);
+            label6.Location = new Point(91, 464);
+            label6.Name = "label6";
+            label6.Size = new Size(97, 17);
+            label6.TabIndex = 5;
+            label6.Text = "Back to LOGIN";
+            // 
+            // kryptonButton1
+            // 
+            kryptonButton1.ButtonStyle = Krypton.Toolkit.ButtonStyle.LowProfile;
+            kryptonButton1.CornerRoundingRadius = -1F;
+            kryptonButton1.Location = new Point(245, 12);
+            kryptonButton1.Name = "kryptonButton1";
+            kryptonButton1.Size = new Size(28, 25);
+            kryptonButton1.StatePressed.Back.Color1 = Color.Red;
+            kryptonButton1.StatePressed.Back.Color2 = Color.Red;
+            kryptonButton1.StateTracking.Back.Color1 = Color.FromArgb(255, 128, 128);
+            kryptonButton1.StateTracking.Back.Color2 = Color.FromArgb(255, 128, 128);
+            kryptonButton1.StateTracking.Border.Color1 = Color.FromArgb(255, 128, 128);
+            kryptonButton1.StateTracking.Border.Color2 = Color.FromArgb(255, 128, 128);
+            kryptonButton1.StateTracking.Border.ColorAlign = Krypton.Toolkit.PaletteRectangleAlign.Local;
+            kryptonButton1.StateTracking.Border.ColorAngle = 1F;
+            kryptonButton1.StateTracking.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
+            kryptonButton1.TabIndex = 22;
+            kryptonButton1.Values.Text = "X";
+            // 
+            // kryptonNumericUpDown1
+            // 
+            kryptonNumericUpDown1.Location = new Point(124, 262);
+            kryptonNumericUpDown1.Maximum = new decimal(new int[] { 300, 0, 0, 0 });
+            kryptonNumericUpDown1.Name = "kryptonNumericUpDown1";
+            kryptonNumericUpDown1.PaletteMode = Krypton.Toolkit.PaletteMode.Office365Silver;
+            kryptonNumericUpDown1.Size = new Size(120, 22);
+            kryptonNumericUpDown1.TabIndex = 26;
+            // 
+            // kryptonComboBox1
+            // 
+            kryptonComboBox1.CornerRoundingRadius = -1F;
+            kryptonComboBox1.DropDownWidth = 120;
+            kryptonComboBox1.IntegralHeight = false;
+            kryptonComboBox1.Location = new Point(125, 220);
+            kryptonComboBox1.Name = "kryptonComboBox1";
+            kryptonComboBox1.PaletteMode = Krypton.Toolkit.PaletteMode.Office365Silver;
+            kryptonComboBox1.Size = new Size(120, 21);
+            kryptonComboBox1.StateCommon.ComboBox.Content.TextH = Krypton.Toolkit.PaletteRelativeAlign.Near;
+            kryptonComboBox1.TabIndex = 27;
+            kryptonComboBox1.Text = "Aim";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(33, 302);
+            label7.Name = "label7";
+            label7.Size = new Size(78, 17);
+            label7.TabIndex = 1;
+            label7.Text = "Weight(kg)";
+            // 
+            // kryptonNumericUpDown2
+            // 
+            kryptonNumericUpDown2.Location = new Point(124, 302);
+            kryptonNumericUpDown2.Maximum = new decimal(new int[] { 200, 0, 0, 0 });
+            kryptonNumericUpDown2.Name = "kryptonNumericUpDown2";
+            kryptonNumericUpDown2.PaletteMode = Krypton.Toolkit.PaletteMode.Office365Silver;
+            kryptonNumericUpDown2.Size = new Size(120, 22);
+            kryptonNumericUpDown2.StateNormal.Content.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            kryptonNumericUpDown2.TabIndex = 26;
+            // 
+            // txtPassword
+            // 
+            txtPassword.BackColor = Color.FromArgb(230, 231, 233);
+            txtPassword.BorderStyle = BorderStyle.None;
+            txtPassword.Font = new Font("MS UI Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txtPassword.Location = new Point(36, 103);
+            txtPassword.Multiline = true;
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.Size = new Size(218, 28);
+            txtPassword.TabIndex = 28;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.FromArgb(230, 231, 233);
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("MS UI Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(36, 103);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(218, 28);
+            textBox2.TabIndex = 30;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(33, 73);
+            label4.Name = "label4";
+            label4.Size = new Size(75, 17);
+            label4.TabIndex = 29;
+            label4.Text = "First Name";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(33, 145);
+            label8.Name = "label8";
+            label8.Size = new Size(73, 17);
+            label8.TabIndex = 29;
+            label8.Text = "Last Name";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(230, 231, 233);
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("MS UI Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(36, 175);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(218, 28);
+            textBox1.TabIndex = 30;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.FromArgb(230, 231, 233);
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Font = new Font("MS UI Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(36, 175);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.PasswordChar = '*';
+            textBox3.Size = new Size(218, 28);
+            textBox3.TabIndex = 28;
+            // 
+            // FormRegisterAim
+            // 
+            AutoScaleDimensions = new SizeF(8F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(285, 513);
+            ControlBox = false;
+            Controls.Add(textBox3);
+            Controls.Add(txtPassword);
+            Controls.Add(textBox1);
+            Controls.Add(textBox2);
+            Controls.Add(label8);
+            Controls.Add(label4);
+            Controls.Add(kryptonComboBox1);
+            Controls.Add(kryptonNumericUpDown2);
+            Controls.Add(kryptonNumericUpDown1);
+            Controls.Add(kryptonButton1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(clearButton);
+            Controls.Add(registrationButton);
+            Controls.Add(label7);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(label3);
+            Font = new Font("Nirmala UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            ForeColor = Color.FromArgb(164, 165, 169);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4);
+            Name = "FormRegisterAim";
+            StartPosition = FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)kryptonComboBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label3;
+        private Label label1;
+        private Label label2;
+        private Button registrationButton;
+        private Button clearButton;
+        private Label label5;
+        private Label label6;
+        private Krypton.Toolkit.KryptonButton kryptonButton1;
+        private Krypton.Toolkit.KryptonNumericUpDown kryptonNumericUpDown1;
+        private Krypton.Toolkit.KryptonComboBox kryptonComboBox1;
+        private Label label7;
+        private Krypton.Toolkit.KryptonNumericUpDown kryptonNumericUpDown2;
+        private TextBox txtPassword;
+        private TextBox textBox2;
+        private Label label4;
+        private Label label8;
+        private TextBox textBox1;
+        private TextBox textBox3;
+    }
+}
---
+++ FormUI/FormVerification.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace FormUI
+{
+    partial class FormVerification
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label3 = new Label();
+            registrationButton = new Button();
+            kryptonButton1 = new Krypton.Toolkit.KryptonButton();
+            txtPassword = new TextBox();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("MS UI Gothic", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.FromArgb(32, 191, 107);
+            label3.Location = new Point(25, 30);
+            label3.Name = "label3";
+            label3.Size = new Size(154, 27);
+            label3.TabIndex = 0;
+            label3.Text = "Verification";
+            // 
+            // registrationButton
+            // 
+            registrationButton.BackColor = Color.FromArgb(32, 191, 107);
+            registrationButton.Cursor = Cursors.Hand;
+            registrationButton.FlatAppearance.BorderSize = 0;
+            registrationButton.FlatStyle = FlatStyle.Flat;
+            registrationButton.ForeColor = Color.White;
+            registrationButton.Location = new Point(34, 179);
+            registrationButton.Name = "registrationButton";
+            registrationButton.Size = new Size(218, 28);
+            registrationButton.TabIndex = 4;
+            registrationButton.Text = "REGISTER";
+            registrationButton.UseVisualStyleBackColor = false;
+            // 
+            // kryptonButton1
+            // 
+            kryptonButton1.ButtonStyle = Krypton.Toolkit.ButtonStyle.LowProfile;
+            kryptonButton1.CornerRoundingRadius = -1F;
+            kryptonButton1.Location = new Point(245, 12);
+            kryptonButton1.Name = "kryptonButton1";
+            kryptonButton1.Size = new Size(28, 25);
+            kryptonButton1.StatePressed.Back.Color1 = Color.Red;
+            kryptonButton1.StatePressed.Back.Color2 = Color.Red;
+            kryptonButton1.StateTracking.Back.Color1 = Color.FromArgb(255, 128, 128);
+            kryptonButton1.StateTracking.Back.Color2 = Color.FromArgb(255, 128, 128);
+            kryptonButton1.StateTracking.Border.Color1 = Color.FromArgb(255, 128, 128);
+            kryptonButton1.StateTracking.Border.Color2 = Color.FromArgb(255, 128, 128);
+            kryptonButton1.StateTracking.Border.ColorAlign = Krypton.Toolkit.PaletteRectangleAlign.Local;
+            kryptonButton1.StateTracking.Border.ColorAngle = 1F;
+            kryptonButton1.StateTracking.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
+            kryptonButton1.TabIndex = 22;
+            kryptonButton1.Values.Text = "X";
+            // 
+            // txtPassword
+            // 
+            txtPassword.BackColor = Color.FromArgb(230, 231, 233);
+            txtPassword.BorderStyle = BorderStyle.None;
+            txtPassword.Font = new Font("MS UI Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txtPassword.Location = new Point(36, 115);
+            txtPassword.Multiline = true;
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.Size = new Size(218, 28);
+            txtPassword.TabIndex = 28;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.FromArgb(230, 231, 233);
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("MS UI Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(36, 115);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(218, 28);
+            textBox2.TabIndex = 30;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(33, 85);
+            label4.Name = "label4";
+            label4.Size = new Size(114, 17);
+            label4.TabIndex = 29;
+            label4.Text = "Verification Code";
+            // 
+            // FormVerification
+            // 
+            AutoScaleDimensions = new SizeF(8F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(285, 242);
+            ControlBox = false;
+            Controls.Add(txtPassword);
+            Controls.Add(textBox2);
+            Controls.Add(label4);
+            Controls.Add(kryptonButton1);
+            Controls.Add(registrationButton);
+            Controls.Add(label3);
+            Font = new Font("Nirmala UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            ForeColor = Color.FromArgb(164, 165, 169);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4);
+            Name = "FormVerification";
+            StartPosition = FormStartPosition.CenterScreen;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label3;
+        private Button registrationButton;
+        private Krypton.Toolkit.KryptonButton kryptonButton1;
+        private TextBox txtPassword;
+        private TextBox textBox2;
+        private Label label4;
+    }
+}
