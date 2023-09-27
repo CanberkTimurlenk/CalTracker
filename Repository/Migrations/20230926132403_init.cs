4e3ef0823c7c87be0b5663ddf99bc37cc5aeb1bb
CanberkTimurlenk <canberk.timurlenk@gmail.com>
2023-09-27 16:15:23 +0300

    Resolve Conflicts

---
+++ Repository/Migrations/20230926132403_init.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AimName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aims", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FoodCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoodCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserDatas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BodyMassIndex = table.Column<double>(type: "float", nullable: false),
+                    BmiStatus = table.Column<int>(type: "int", nullable: false),
+                    DailyCalorie = table.Column<double>(type: "float", nullable: false),
+                    DailyFat = table.Column<double>(type: "float", nullable: false),
+                    DailyCarbonhidrate = table.Column<double>(type: "float", nullable: false),
+                    DailyProtein = table.Column<double>(type: "float", nullable: false),
+                    RequiredCalorie = table.Column<double>(type: "float", nullable: false),
+                    WaterAmount = table.Column<double>(type: "float", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserDatas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserAims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AimId = table.Column<int>(type: "int", nullable: false),
+                    Coefficient = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserAims_Aims_AimId",
+                        column: x => x.AimId,
+                        principalTable: "Aims",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Foods",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Calorie = table.Column<double>(type: "float", nullable: false),
+                    Fat = table.Column<double>(type: "float", nullable: false),
+                    Carbonhidrate = table.Column<double>(type: "float", nullable: false),
+                    Protein = table.Column<double>(type: "float", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImagePath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FoodCategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Foods", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Foods_FoodCategories_FoodCategoryId",
+                        column: x => x.FoodCategoryId,
+                        principalTable: "FoodCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Height = table.Column<double>(type: "float", nullable: false),
+                    Weight = table.Column<double>(type: "float", nullable: false),
+                    UserStatus = table.Column<int>(type: "int", nullable: false),
+                    UserDataId = table.Column<int>(type: "int", nullable: false),
+                    UserAimId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_UserAims_UserAimId",
+                        column: x => x.UserAimId,
+                        principalTable: "UserAims",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Users_UserDatas_UserDataId",
+                        column: x => x.UserDataId,
+                        principalTable: "UserDatas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserMeals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserMealsID = table.Column<int>(type: "int", nullable: false),
+                    MealDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MealTimes = table.Column<int>(type: "int", nullable: false),
+                    Calorie = table.Column<double>(type: "float", nullable: false),
+                    Fat = table.Column<double>(type: "float", nullable: false),
+                    Carbonhidrate = table.Column<double>(type: "float", nullable: false),
+                    Protein = table.Column<double>(type: "float", nullable: false),
+                    Portion = table.Column<double>(type: "float", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    UserDataId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserMeals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserMeals_UserDatas_UserDataId",
+                        column: x => x.UserDataId,
+                        principalTable: "UserDatas",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_UserMeals_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserVerifications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Verification = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserVerifications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserVerifications_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FoodUserMeals",
+                columns: table => new
+                {
+                    FoodsId = table.Column<int>(type: "int", nullable: false),
+                    UserMealsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoodUserMeals", x => new { x.FoodsId, x.UserMealsId });
+                    table.ForeignKey(
+                        name: "FK_FoodUserMeals_Foods_FoodsId",
+                        column: x => x.FoodsId,
+                        principalTable: "Foods",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FoodUserMeals_UserMeals_UserMealsId",
+                        column: x => x.UserMealsId,
+                        principalTable: "UserMeals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Foods_FoodCategoryId",
+                table: "Foods",
+                column: "FoodCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FoodUserMeals_UserMealsId",
+                table: "FoodUserMeals",
+                column: "UserMealsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAims_AimId",
+                table: "UserAims",
+                column: "AimId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserMeals_UserDataId",
+                table: "UserMeals",
+                column: "UserDataId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserMeals_UserId",
+                table: "UserMeals",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserAimId",
+                table: "Users",
+                column: "UserAimId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserDataId",
+                table: "Users",
+                column: "UserDataId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserVerifications_UserId",
+                table: "UserVerifications",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FoodUserMeals");
+
+            migrationBuilder.DropTable(
+                name: "UserVerifications");
+
+            migrationBuilder.DropTable(
+                name: "Foods");
+
+            migrationBuilder.DropTable(
+                name: "UserMeals");
+
+            migrationBuilder.DropTable(
+                name: "FoodCategories");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "UserAims");
+
+            migrationBuilder.DropTable(
+                name: "UserDatas");
+
+            migrationBuilder.DropTable(
+                name: "Aims");
+        }
+    }
+}
