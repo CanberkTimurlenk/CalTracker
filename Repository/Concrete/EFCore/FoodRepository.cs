3f1b0d71b3b13aa56b7ff4820600fce28378bdd8
batuhanbora <batuhanbora.alaftar@bilgeadamboost.com>
2023-09-27 15:33:44 +0300

    Create Repositories

---
+++ Repository/Concrete/EFCore/FoodRepository.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using Repository.Abstract.Base;
+using Repository.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Concrete.EFCore
+{
+    public class FoodRepository : BaseRepository<Food>, IFoodRepository
+    {
+    }
+}
