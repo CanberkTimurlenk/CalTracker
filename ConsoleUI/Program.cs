67cf6a25a57ed0bfe990e30e0b73dd8dc9202ca5
batuhanbora <batuhanbora.alaftar@bilgeadamboost.com>
2023-09-27 16:14:46 +0300

    Resolve Conflicts

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Context;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KaloriTakipDbContext context = new KaloriTakipDbContext();
+
+            foreach (var item in context.UserDatas)
+            {
+                Console.WriteLine(item.BmiStatus.ToString());
+            }
+        }
+    }
+}
