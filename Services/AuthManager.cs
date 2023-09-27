617d0e50f2f37ca93b34ac0724578b92d940b4d3
CanberkTimurlenk <canberk.timurlenk@gmail.com>
2023-09-27 15:38:12 +0300

    Add AuthService

---
+++ Services/AuthManager.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Concrete;
+using Entities.Dtos;
+using Entities.Exceptions;
+
+namespace Services
+{
+    public class AuthManager : IAuthService
+    {
+        private readonly IUserService _userService = new UserManager();
+        public User Login(UserForLoginDto userForLogin)
+        {
+            var user = _userService.GetByEmail(userForLogin.Email);
+
+            if (!user.Password.Equals(userForLogin.Password))
+                throw new WrongCredentialsException(userForLogin.Email);
+
+            return user;
+        }
+
+        public User Register(UserForRegisterDto userForRegister)
+        {
+            _userService.Add(user);
+
+            return null;
+        }
+
+
+    }
+}
