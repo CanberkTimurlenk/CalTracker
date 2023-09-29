410fd84df5c0f4309644931535e9b0268e5a3211
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-09-29 11:24:29 +0300

    Update Password

--- Entities/Concrete/User.cs
+++ Entities/Concrete/User.cs
@@ -6,12 +6,13 @@ namespace Entities.Concrete
     public class User : BaseEntity, IEntity
     {
         public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string LastName { get; set; } 
+        public string Email { get; set; }    
+        public byte[] PasswordHash { get; set; } 
+        public byte[] PasswordSalt { get; set; } 
         public double Height { get; set; }
         public double Weight { get; set; }
-        public UserStatus UserStatus { get; set; } 
+        public UserStatus UserStatus { get; set; }
 
         public int AimId { get; set; }
         public Aim Aim { get; set; }
